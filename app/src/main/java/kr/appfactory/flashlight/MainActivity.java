0d3c7417d3d73fd0c420765a85ed67468ff71cd7
MiSamChoi <cms736@naver.com>
2019-06-24 13:30:11 +0900

    수정

--- app/src/main/java/kr/appfactory/flashlight/MainActivity.java
+++ app/src/main/java/kr/appfactory/flashlight/MainActivity.java
@@ -1,187 +1,80 @@
 package kr.appfactory.flashlight;
 
-import android.Manifest;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.os.Handler;
-import android.support.annotation.NonNull;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Surface;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private CameraManager mCameraManager;
-    private CaptureRequest.Builder mPreviewRequestBuilder;
-    private CameraCaptureSession mCaptureSession;
-    private CameraDevice mCameraDevice;
-    private SurfaceTexture mTexture;
-    private String mCameraId;
-    private ImageButton mImageButtonFlashOnOff;
-    private boolean mFlashOn;
+    static  Camera camera = null;
+    private Context mContext=MainActivity.this;
+    private static final int REQUEST = 112;
 
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-            Toast.makeText(getApplicationContext(), "There is no camera flash.\n The app will finish!", Toast.LENGTH_LONG).show();
-            delayedFinish();
-            return;
+        if (Build.VERSION.SDK_INT >= 23) {
+            String[] PERMISSIONS = {android.Manifest.permission.CAMERA};
+            if (!hasPermissions(mContext, PERMISSIONS)) {
+                ActivityCompat.requestPermissions((Activity) mContext, PERMISSIONS, REQUEST );
+            } else {
+                //do here
+            }
+        } else {
+            //do here
         }
 
-        mCameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        setContentView(R.layout.activity_main);
+        camera = Camera.open();
+    }
 
-        mImageButtonFlashOnOff = findViewById(R.id.ibFlashOnOff);
-        mImageButtonFlashOnOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flashlight();
-                mImageButtonFlashOnOff.setImageResource(mFlashOn ? android.R.drawable.btn_star_big_on : android.R.drawable.btn_star_big_off);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        camera.release();
+    }
+
+    public void onClick(View view) {
+
+            if(view.getId() == R.id.btn_flash_on){
+                Camera.Parameters param = camera.getParameters();
+                param.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(param);
+                camera.startPreview();
+            }else if(view.getId() == R.id.btn_flash_off){
+                Camera.Parameters param = camera.getParameters();
+                param.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                camera.setParameters(param);
+                camera.stopPreview();
             }
-        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        openCamera();
     }
 
     @Override
     protected void onPause() {
-        closeCamera();
         super.onPause();
     }
 
-    private void openCamera() {
-        try {
-            for (String id : mCameraManager.getCameraIdList()) {
-                CameraCharacteristics c = mCameraManager.getCameraCharacteristics(id);
-                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
-                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
-                if (flashAvailable != null && flashAvailable
-                        && lensFacing != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
-                    mCameraId = id;
-                    break;
-                }
-            }
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            mCameraManager.openCamera(mCameraId, new CameraDevice.StateCallback() {
-                @Override
-                public void onOpened(@NonNull CameraDevice cameraDevice) {
-                    mCameraDevice = cameraDevice;
-
-                    try {
-                        List<Surface> list = new ArrayList<>();
-                        if (mTexture != null) {
-                            mTexture.release();
-                        }
-                        mTexture = new SurfaceTexture(1);
-                        Surface surface = new Surface(mTexture);
-                        list.add(surface);
-                        mPreviewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                        mPreviewRequestBuilder.addTarget(surface);
-                        cameraDevice.createCaptureSession(list, new CameraCaptureSession.StateCallback() {
-                            @Override
-                            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                                if (null == mCameraDevice) {
-                                    return;
-                                }
-                                try {
-                                    mCaptureSession = cameraCaptureSession;
-                                    cameraCaptureSession.setRepeatingRequest(mPreviewRequestBuilder.build(), null, null);
-                                } catch (CameraAccessException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            @Override
-                            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                            }
-                        }, null);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onDisconnected(@NonNull CameraDevice cameraDevice) {
-                    cameraDevice.close();
-                    mCameraDevice = null;
-                }
-
-                @Override
-                public void onError(@NonNull CameraDevice cameraDevice, int error) {
-                    cameraDevice.close();
-                    mCameraDevice = null;
-                    Toast.makeText(getApplicationContext(), "A camera error has occurred. The app will finish!", Toast.LENGTH_LONG).show();
-                    delayedFinish();
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void closeCamera() {
-        if (null != mTexture) {
-            mTexture.release();
-            mTexture = null;
-        }
-
-        if (null != mCaptureSession) {
-            mCaptureSession.close();
-            mCaptureSession = null;
-        }
-
-        if (null != mCameraDevice) {
-            mCameraDevice.close();
-            mCameraDevice = null;
-        }
-    }
-
-    private void flashlight() {
-        mFlashOn = !mFlashOn;
-
-        mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE, mFlashOn ? CaptureRequest.FLASH_MODE_TORCH : CaptureRequest.FLASH_MODE_OFF);
-
-        try {
-            mCaptureSession.setRepeatingRequest(mPreviewRequestBuilder.build(), null, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void delayedFinish() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                finish();
-            }
-        }, 3000);
-    }
 }
